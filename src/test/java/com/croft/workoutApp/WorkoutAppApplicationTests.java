3bf0aba0a40f0f6cf470fd44d24bcc020639128f
jmcroft7 <croftmjohn@gmail.com>
2022-06-15 13:36:20 -0500

    init

---
+++ src/test/java/com/croft/workoutApp/WorkoutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.croft.workoutApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
